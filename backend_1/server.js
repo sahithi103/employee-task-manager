606d4afe87edaf4cde4930dd682be970b953d906
varsha sahithi <yvarshaa1@gmail.com>
2025-11-27 00:10:37 +0530

    connected database

--- backend_1/server.js
+++ backend_1/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cors from "cors";
+import employeeRoutes from "./routes/employeeRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +14,12 @@ const app = express();
 
 // Middleware to read JSON
 app.use(express.json());
+
+app.use(cors());
+
+
+app.use("/api/employees", employeeRoutes);
+app.use("/api/tasks", taskRoutes);
 
 // Basic route
 app.get("/", (req, res) => {
